3e585e9d26cb31a2d223fadf0e226427c7f880f0
Meaghan <meaghan.sharrard89@gmail.com>
2023-11-12 13:21:52 -0800

    pass tests

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9\s'-]/g, '');
+  }
+
+  static titleize(string){
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const array = string.split(' ');
+    const capitalizedArray = array.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word.toLowerCase())) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word.toLowerCase();
+      }
+    })
+    return capitalizedArray.join(' ');
+  }
 }
